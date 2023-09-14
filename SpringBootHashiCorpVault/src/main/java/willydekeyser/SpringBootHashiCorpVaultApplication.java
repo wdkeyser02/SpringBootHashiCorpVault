77b59caff885bb085846d1e61fd0ad9b55af3a84
willydekeyser <ma074172@skynet.be>
2023-09-14 19:51:08 +0200

    Update

--- SpringBootHashiCorpVault/src/main/java/willydekeyser/SpringBootHashiCorpVaultApplication.java
+++ SpringBootHashiCorpVault/src/main/java/willydekeyser/SpringBootHashiCorpVaultApplication.java
@@ -1,13 +1,33 @@
 package willydekeyser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
 
 @SpringBootApplication
 public class SpringBootHashiCorpVaultApplication {
 
+	@Autowired
+	private VaultTemplate vaultTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHashiCorpVaultApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			VaultResponse response = vaultTemplate
+			        .opsForKeyValue("admin/secret", KeyValueBackend.KV_2).get("example");
+			System.out.println(response.getData());
+			System.out.println(response.getData().get("username"));
+			System.out.println(response.getData().get("password"));
+			
+		};
+	}
 }
