196309a32b8ffa667a0d2157090e646870cbdf78
willydekeyser <ma074172@skynet.be>
2023-09-20 15:47:06 +0200

    Update properties

--- SpringBootHashiCorpVault/src/main/java/willydekeyser/SpringBootHashiCorpVaultApplication.java
+++ SpringBootHashiCorpVault/src/main/java/willydekeyser/SpringBootHashiCorpVaultApplication.java
@@ -1,13 +1,34 @@
 package willydekeyser;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootHashiCorpVaultApplication {
+
+	@Value("${username}")
+    String userName;
+
+    @Value("${password}")
+    String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHashiCorpVaultApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("CommandLineRunner");
+			System.err.println(userName);	
+			System.err.println(password);	
+			
+			
+		};
+	}
 }
+
+
